9bd946d3ee4bfab3238b82f733de0c96ee0c965e
David B <37987466+dalijolijo@users.noreply.github.com>
2018-05-27 22:24:07 +0200

    Create extractUsers.sh

---
+++ extractUsers.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# extractUsers.sh (by dalijolijo - 05/2018)
+#
+
+# Sync with NTP server
+# -> install with: apt-get install ntp
+service ntp stop
+service ntp start
+sleep 10
+
+# Export members of the telegram channel 
+# -> install with: apt-get install python3
+python3 get_participants.py > users_all.json
+
+# Extract only first_name, last_name and username
+cat users_all.json | grep first_name | tr -d " " | tr -d "," | cut -f 2 -d ":" > first_name.txt
+cat users_all.json | grep last_name | tr -d " " | tr -d "," | cut -f 2 -d ":" > last_name.txt
+cat users_all.json | grep username | tr -d " " | tr -d "," | cut -f 2 -d ":" > username.txt
+paste -d "_" first_name.txt last_name.txt username.txt > userString.txt
+
+# Participants to identified
+USER_BEFORE=$(cat userString.txt | wc -l)
+printf "Participants (all): $USER_BEFORE \n"
+sed -i '/null_null_null/d' userString.txt
+USER_AFTER=$(cat userString.txt | wc -l)
+printf "Participants (identified): $USER_AFTER \n"
+cp userString.txt userString_$(date +%d-%h_%H:%M).txt
