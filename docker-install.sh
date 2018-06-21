e2aca8998f18ac27e1efb1b8dc79a938e78f3eee
David B <37987466+dalijolijo@users.noreply.github.com>
2018-06-21 15:30:38 +0200

    Create docker-install.sh

---
+++ docker-install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -u
+
+DOCKER_REPO="dalijolijo"
+CHANNEL="coin_analyse"
+
+#
+# Set telegram user pwd
+#
+echo -n "Enter new password for [telegram] user and Hit [ENTER]: "
+read TELEPWD
+
+#
+# Set telegram API data
+#
+echo -n "Enter your Telegram API ID and Hit [ENTER]: "
+read API_ID
+echo -n "Enter your Telegram API Hash and Hit [ENTER]: "
+read API_HASH
+echo -n "Enter your Telegram Channel/Chat (e.g. coin_analyse) which you want to export users and Hit [ENTER]: "
+read CHANNEL
+
+#
+# Installation of docker-ce package (Ubuntu 16.04)
+#
+apt-get update
+sudo curl -sSL https://get.docker.com | sh
+
+#
+# Pull telegram-get-participants
+#
+docker pull ${DOCKER_REPO}/https://github.com/dalijolijo/telegram-get-participants.git
+docker run --name telegram-export -e TELEPWD="${TELEPWD}" -e API_ID="${API_ID}" -e API_HASH="${API_HASH}" -e CHANNEL="${CHANNEL}" -v /home/telegram:/home/telegram:rw -d ${DOCKER_REPO}/telegram-get-participants
