a6b7dcc2a2cd63d9e34403c510c66e06979aedaf
David B <37987466+dalijolijo@users.noreply.github.com>
2018-06-21 21:11:06 +0200

    Remove CONFIG variable

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -u
 
-CONFIG='get_participants.py'
-
 #
 # Set passwd of telegram user
 #
@@ -15,25 +13,25 @@ chown -R telegram:telegram /home/telegram
 #
 cd /tmp/
 wget https://github.com/dalijolijo/telegram-get-participants/raw/master/extractUsers.sh --output-document=/tmp/extractUsers.sh
-wget https://github.com/dalijolijo/telegram-get-participants/raw/master/${CONFIG} --output-document=/tmp/${CONFIG}
+wget https://github.com/dalijolijo/telegram-get-participants/raw/master/get_participants.py --output-document=/tmp/get_participants.py
 chown telegram:telegram /tmp/extractUsers.sh
-chown telegram:telegram /tmp/${CONFIG}
+chown telegram:telegram /tmp/get_participants.py
 chown -R telegram:telegram /home/telegram/
 sudo -u telegram cp /tmp/extractUsers.sh /home/telegram/extractUsers.sh
-sudo -u telegram cp /tmp/${CONFIG} /home/telegram/${CONFIG}
+sudo -u telegram cp /tmp/get_participants.py /home/telegram/get_participants.py
 
 #
 # Set Telegram API developer data
 #
-sed -i s/API_ID/${API_ID}/g /home/telegram/${CONFIG}
-sed -i s/API_HASH/${API_HASH}/g /home/telegram/${CONFIG}
-sed -i s/CHANNEL/${CHANNEL}/g /home/telegram/${CONFIG}
+sed -i s/API_ID/${API_ID}/g /home/telegram/get_participants.py
+sed -i s/API_HASH/${API_HASH}/g /home/telegram/get_participants.py
+sed -i s/CHANNEL/${CHANNEL}/g /home/telegram/get_participants.py
 
 #
 # Execution of extractUsers script
 #
 sudo -u telegram +x /home/telegram/extractUsers.sh
-sudo -u telegram +x /home/telegram/${CONFIG}
+sudo -u telegram +x /home/telegram/get_participants.py
 sudo -u telegram /home/telegram/extractUsers.sh
 
 #
