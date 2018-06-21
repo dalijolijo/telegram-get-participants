3b2bdb4de8bb6eee1ca2bbfc702d31c63932aacc
David B <37987466+dalijolijo@users.noreply.github.com>
2018-06-21 15:23:23 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -u
+
+CONFIG='get_participants.py'
+
+#
+# Set passwd of telegram user
+#
+echo telegram:${TELEPWD} | chpasswd
+mkdir -p /home/telegram/
+chown -R telegram:telegram /home/telegram
+
+#
+# Set Telegram API developer data
+#
+sed -i s/API_ID/${API_ID}/g /root/telegram-get-participants/${CONFIG}
+sed -i s/API_HASH/${API_HASH}/g /root/telegram-get-participants/${CONFIG}
+sed -i s/CHANNEL/${CHANNEL}/g /root/telegram-get-participants/${CONFIG}
+
+#
+# Starting Supervisor Service
+#
+# Hint: docker not supported systemd, use of supervisord
+printf "*** Starting Supervisor Service ***\n"
+exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
